88a5424db2cd05f2a17b54285e77ec9a35c3d7b6
Fernanda Larocca <fernandacolacovaz@hotmail.com>
2024-05-01 15:14:34 -0300

    Implementando funcionalidade que ao marcar o livro como devolvido a quantidade que fora emprestada é adicionada ao etoque aotomaticamente

--- app/Actions/Loan/UpdateLoanAction.php
+++ app/Actions/Loan/UpdateLoanAction.php
@@ -3,12 +3,17 @@
 namespace App\Actions\Loan;
 
 use App\Http\Resources\LoanResource;
+use App\Models\Book;
 use App\Models\Loan;
 
 class UpdateLoanAction
 {
     public function execute(array $data, Loan $loan): LoanResource
     {
+        if (isset($data['rebounded_book']) && $data['rebounded_book'] && !$loan->rebounded_book) {
+            $book = Book::findOrFail($loan->book_id);
+            $book->increment('quantity_in_stock', $loan->quantity);
+        }
         $loan->update($data);
         return LoanResource::make($loan);
     }
