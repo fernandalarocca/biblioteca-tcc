a889908a9fdc0761b656353aab668bcd4a44a0f8
Fernanda Larocca <fernandacolacovaz@hotmail.com>
2024-05-06 19:35:40 -0300

    Traduzindo as mensagens de erro

---
+++ resources/lang/pt_BR/validation.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'required' => 'O campo :attribute é obrigatório.',
+    'email' => 'O campo :attribute deve ser um endereço de email válido.',
+    'max' => [
+        'numeric' => 'O campo :attribute não deve ser maior que :max.',
+        'file' => 'O arquivo :attribute não deve ser maior que :max kilobytes.',
+        'string' => 'O campo :attribute não deve ter mais que :max caracteres.',
+        'array' => 'O campo :attribute não deve ter mais que :max itens.',
+    ],
+    'unique' => 'O :attribute já está em uso.',
+
+    'attributes' => [
+        'first_name' => 'nome',
+        'last_name' => 'sobrenome',
+        'age' => 'idade',
+        'description' => 'descrição',
+        'title' => 'título',
+        'synopsis' => 'sinopse',
+        'category' => 'categoria',
+        'published_at' => 'data de publicação',
+        'quantity_in_stock' => 'quantidade em estoque',
+        'author_id' => 'autor',
+        'book_in' => 'livro',
+        'quantity' => 'quantidade',
+        'clients_name' => 'nome do cliente',
+        'cpf' => 'cpf',
+        'phone' => 'telefone',
+        'date_to_return_book' => 'data de devolução',
+        'rebounded_book' => 'livro de devolvido',
+        'name' => 'nome',
+        'email' => 'email',
+        'password' => 'senha',
+        'is_admin' => 'administrador',
+    ],
+];
