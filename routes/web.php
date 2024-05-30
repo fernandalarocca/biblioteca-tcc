0ad6c79e288a7b8a878866f1baef0e7f88f3085b
Fernanda Larocca <fernandacolacovaz@hotmail.com>
2024-05-29 22:19:30 -0300

    Ajustando arquivo de rotas

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/home');
+    return redirect('/public/home');
 });
 
 Route::get('/home', function () {
